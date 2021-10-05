70095604d61cb84491f6ed67399ee7519ce73437
ragsfdc <91958666+ragsfdc@users.noreply.github.com>
2021-10-05 22:19:11 +0530

    Update and rename test.py__ to test.py

--- test.py
+++ test.py
@@ -6,6 +6,14 @@ import datetime
 import logging
 import pandas as pd
 import numpy as np
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 logging.basicConfig(level=logging.DEBUG)
 bull_out=pd.DataFrame()
